035c8e017de4e600dc28111a04b563fb8d09062c
ImShekhar17 <Shekharsharma17800@gmail.com>
2023-03-14 20:45:18 +0530

    shekhar

---
+++ slider-data.js
@@ -0,0 +1,32 @@
+let movies = [
+    {
+        name: 'WI  vs IND 317/8 (50)',
+        des: 'Watch this Amazing ground firing match',
+        image: 'images/slider 6.PNG'
+    },
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Following the events of "Avengers: Endgame,"Sam Willson and Bucky Branes team up in a global adventure that tests their abilities and their patience.',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of "Avengers: Endgame.',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Wanda Maximoff and Vision-two super-powered beings living idealized suburban lives--begin to suspect that everything is not as it seems.',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'The movie is coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides.',
+        image: 'images/slider 5.PNG'
+    }
+]
